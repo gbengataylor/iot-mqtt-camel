10a7e6b5ffe11e1e455bcba79f782c69b9a6a151
rhn-support-adtaylor <rhn-support-adtaylor@che>
2022-10-13 00:26:51 +0000

    added mqtt

--- producer/src/main/java/org/acme/ProducerRoute.java
+++ producer/src/main/java/org/acme/ProducerRoute.java
@@ -9,10 +9,11 @@ public class ProducerRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("timer:GenerateVitalSigns?period=1000")
+        from("timer:GenerateVitalSigns?period={{message.period}}")
             .bean("vitalsigns" , "generate")
             .marshal().json()
-            .log("${body}");    
+            .log("${body}")
+            .to("paho-mqtt5:{{mqtt.producer.topic}}?brokerURL={{mqtt.producer.url}}");
     }
 
 }
